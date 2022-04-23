ea6158f0b4700fe12aea25516960c61e53280856
erin <erinverkoulen93@gmail.com>
2022-04-23 00:01:40 -0500

    changed wss link

--- client/src/pages/MessageForm.js
+++ client/src/pages/MessageForm.js
@@ -32,9 +32,9 @@ function MessageForm({user, selectedConvo }) {
 
     useEffect(() => {
         if(selectedConvo) {
-            const socket = new WebSocket( "ws://localhost:3000/cable"
+            const socket = new WebSocket(  "wss://fast-reaches-73823.herokuapp.com/cable"
             )
-            // "wss://fast-reaches-73823.herokuapp.com/cable"
+           
             socket.addEventListener("open", (event) => {
                 const message = {
                     command: "subscribe",
